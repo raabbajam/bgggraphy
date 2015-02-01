46c3f9ba1c453a125830a273691a3e642eae83a1
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2015-02-02 06:05:34 +0700

    Removed unnecessary assets

--- dists/js/timeline.js
+++
@@ -1,1 +0,0 @@
-!function(){var n=$("#timeline"),i=$("ul > li",n);console.log("lists",i),i.hover(function(){console.log("enter"),$(this).addClass("active")},function(){console.log("out"),$(this).one("webkitAnimationEnd oanimationend msAnimationEnd animationend",function(){$(this).removeClass("active")})})}();
